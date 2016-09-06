5d67de36f5fece1a703d481a471bddcb46edf23b
carl12 <carl.rodriguez13@gmail.com>
2016-09-06 11:05:27 -0700

    Make figure pretty

--- sampleRun.m
+++ sampleRun.m
@@ -3,14 +3,19 @@
 
 
 
-
+%%Load Data
 bodytemp = load('bodytemp.dat');
-
 [row,col] = size(bodytemp);
 x = 1:row;
+
+%%Create Figure and display
 fig1 = figure
 fig1.Color = 'y';
 fig1.Name = 'Body Temp';
 
 
-scatter(x,bodytemp, 20, 'p');
+myPlot = scatter(x,bodytemp, 10000, 'p');
+title('Scatter plot for body temp','FontSize',20,'FontWeight','bold');
+xlabel('Beotches','FontSize',16,'FontWeight','bold');
+ylabel('Hotness','FontSize',16,'FontWeight','bold');
+
