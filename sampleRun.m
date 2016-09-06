9c53d2f717f453d5dc7f63329b09b50eceb7fd85
carl12 <carl.rodriguez13@gmail.com>
2016-09-06 11:09:38 -0700

    Get mean and var for male and female

--- sampleRun.m
+++ sampleRun.m
@@ -9,13 +9,26 @@ bodytemp = load('bodytemp.dat');
 x = 1:row;
 
 %%Create Figure and display
-fig1 = figure
+fig1 = figure;
 fig1.Color = 'y';
 fig1.Name = 'Body Temp';
 
 
-myPlot = scatter(x,bodytemp, 10000, 'p');
+myPlot = scatter(x,bodytemp, 100000, 'p');
 title('Scatter plot for body temp','FontSize',20,'FontWeight','bold');
-xlabel('Beotches','FontSize',16,'FontWeight','bold');
-ylabel('Hotness','FontSize',16,'FontWeight','bold');
+xlabel('People','FontSize',16,'FontWeight','bold');
+ylabel('Temperature (F)','FontSize',16,'FontWeight','bold');
+
+%%Calculate total mean and variance
+total_mean = mean(bodytemp);
+total_var = var(bodytemp);
+
+males = bodytemp(1:65);
+females = bodytemp(66:end);
+
+male_mean = mean(males);
+female_mean = mean(females);
+
+male_var = var(males);
+female_var = var(females);
 
